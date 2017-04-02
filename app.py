c6e32e66be93a91810a08f421b93994b62c31a73
rohitcmohan <rohitcmohan@gmail.com>
2017-04-01 22:41:16 -0400

    random and kmeans sampling

--- app.py
+++ app.py
@@ -4,31 +4,58 @@ from pymongo import MongoClient
 import json
 from bson import json_util
 from bson.json_util import dumps
+import csv
+import random
+import pandas
+import sklearn.cluster as clt
+import matplotlib.pylab as plt
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
+MONGODB_HOST = "localhost"
 MONGODB_PORT = 27017
-DBS_NAME = 'lab2'
-COLLECTION_NAME = 'produce'
-FIELDS = {'state': True, 'year': True, 'pcap': True, 'hwy': True, 'water': True,'util': True,'emp': True,'unemp': True, '_id': False}
+DBS_NAME = "lab2"
+COLLECTION_NAME = "produce"
+FIELDS = {"state": True, "year": True, "pcap": True, "hwy": True, "water": True,"util": True,"emp": True,"unemp": True, "_id": False}
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/lab2/produce")
+@app.route("/lab2/sample")
 def lab2_produce():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=1000)
-    #projects = collection.find(projection=FIELDS)
+    #projects = collection.find(projection=FIELDS, limit=1000)
+    projects = collection.find(projection=FIELDS)
     json_projects = []
+    f = open("static/data/random.csv", "w", newline="")
+    k = ["","_id","state","year","region","pcap","hwy","water","util","pc","gsp","emp","unemp"]
+    writer = csv.DictWriter(f, fieldnames=k)
+    writer.writeheader()
     for project in projects:
+        if(random.random() > 0.3):
+            writer.writerow(project)
         json_projects.append(project)
+
     json_projects = json.dumps(json_projects, default=json_util.default)
+    df = pandas.read_json(json_projects)
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    dataset = df.select_dtypes(include=numerics)
+    plotd = {}
+    for i in range(1, 10):
+        km = clt.KMeans(n_clusters=i).fit(dataset)
+        iner = km.inertia_
+        plotd[i] = iner
+
+    lists = sorted(plotd.items())
+    x, y = zip(*lists)
+
+    plt.plot(x, y)
+    plt.show()
+
     connection.close()
     return json_projects
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
--- static/js/graph.js
+++ static/js/graph.js
@@ -1,8 +1,23 @@
 queue()
-    .defer(d3.json, "/lab2/produce")
+    .defer(d3.json, "/lab2/sample")
     .await(makeGraphs);
 
 function makeGraphs(error, projectsJson) {
   console.log('hi')
-  //console.log(projectsJson)
+  console.log('hello', projectsJson)
+  var data = projectsJson;
+  var dataset = data.map(function(a) {return a.emp;});
+  console.log(dataset)
+
+  var ndx = crossfilter(data)
+  console.log(ndx)
+  var stateDim = ndx.dimension(function(d) { return d["state"]; });
+  var yearDim = ndx.dimension(function(d) { return d["year"]; });
+  console.log(stateDim)
+  var waterState = stateDim.group().reduceSum(function(d) {
+    return d["water"];
+  });
+
+
+
 };
