da319eb09593524ff617b7ff268b9de47e1aa21d
rohitcmohan <rohitcmohan@gmail.com>
2017-04-03 18:16:34 -0400

    d3 visualization for pca and mds

--- app.py
+++ app.py
@@ -8,9 +8,14 @@ import csv
 import random
 import pandas
 import sklearn.cluster as clt
+import sklearn.decomposition as dec
+import sklearn.manifold as man
 import matplotlib.pylab as plt
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import heapq
+from operator import itemgetter
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 
@@ -66,14 +71,16 @@ def lab2_produce():
 
     #PCA
     dataset_strat = strat_df.select_dtypes(include=numerics)
-    dataset_strat.apply(pandas.to_numeric, errors='coerce')
+    del dataset_strat['']
+    cols = list(dataset_strat.columns.values)
+    print(cols)
+    #dataset_strat.apply(pandas.to_numeric, errors='coerce')
     std_data = StandardScaler().fit_transform(dataset_strat)
     mean_vec = np.mean(std_data, axis=0)
     cov_mat = (std_data - mean_vec).T.dot((std_data - mean_vec)) / (std_data.shape[0]-1)
-    #print(std_data)
+    #print(dataset_strat)
     cov_mat = np.cov(std_data.T)
     eigen_vals = np.linalg.eigvals(np.array(cov_mat))
-    # print(eigen_vals)
     #plot eigen_vals
     # i = 1
     # for v in eigen_vals:
@@ -83,10 +90,53 @@ def lab2_produce():
     # x, y = zip(*lists)
     # plt.plot(x, y)
     # plt.show()
-    
+
+    col_eigen = {}
+    for i in range(len(cols)):
+        col_eigen[cols[i]] = eigen_vals[i]
+
+    pca = dec.PCA(n_components=2)
+    pca_cor = pca.fit(std_data).components_
+    pca_mat = pca.fit_transform(std_data)
+    #print(pca_mat)
+    #print(pca_cor)
+    pca_dict = {}
+    #plot pca_vals
+    for i in range(len(pca_cor[0])):
+        plotd[i] = pca_cor[0][i] * pca_cor[0][i] + pca_cor[1][i] * pca_cor[1][i]
+        pca_dict[cols[i]] = plotd[i]
+
+    top_pca = dict(heapq.nlargest(3, pca_dict.items(), key=itemgetter(1)))
+    #print(top_pca)
+    pca_d = {}
+    for i in range(len(pca_mat)):
+        pca_d[i] = {'x':pca_mat[i][0], 'y':pca_mat[i][1], 'label':int(dataset_strat['label'].iloc[i])}
+    #print(pca_d)
+    pca_json = json.dumps(pca_d)
+
+    #MDS
+    #correlattion
+    # mds_corr = 1 - cosine_similarity(std_data)
+    # mds = man.MDS(n_components=2, dissimilarity='precomputed')
+    # mds_mat = mds.fit(mds_corr).embedding_
+
+    #euclidean
+    mds = man.MDS(n_components=2, dissimilarity='euclidean')
+    mds_mat = mds.fit(std_data).embedding_
+
+    mds_d = {}
+    for i in range(len(mds_mat)):
+        mds_d[i] = {'x':mds_mat[i][0], 'y':mds_mat[i][1], 'label':int(dataset_strat['label'].iloc[i])}
+    #print(pca_d)
+    mds_json = json.dumps(mds_d)
 
     connection.close()
-    return strat_df.transpose().to_json()
+    #return pca_json
+    return mds_json
+
+@app.route("/lab2/mds")
+def lab2_mds():
+    return 1
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
--- static/js/graph.js
+++ static/js/graph.js
@@ -3,22 +3,73 @@ queue()
     .await(makeGraphs);
 
 function makeGraphs(error, projectsJson) {
-  console.log('hi')
-  console.log('hello', projectsJson)
-  var data = $.map(projectsJson, function(d) { return d});
+
+  var data = new Array;
+  for(var i=0;i<Object.keys(projectsJson).length;i++) {
+    A = new Array(projectsJson[i].x, projectsJson[i].y, projectsJson[i].label)
+    data.push(A)
+  }
   console.log(data)
-  var dataset = data.map(function(a) {return a.emp;});
-  console.log(dataset)
+  color = ['red', 'blue', 'green']
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-  var ndx = crossfilter(data)
-  console.log(ndx)
-  var stateDim = ndx.dimension(function(d) { return d["state"]; });
-  var yearDim = ndx.dimension(function(d) { return d["year"]; });
-  console.log(stateDim)
-  var waterState = stateDim.group().reduceSum(function(d) {
-    return d["water"];
-  });
+  var xmin = d3.min(data, function(d){return d[0]}),
+  xmax = d3.max(data, function(d){return d[0]}),
+  ymin = d3.min(data, function(d){return d[1]}),
+  ymax = d3.max(data, function(d){return d[1]})
+  //var xValue =function(d) { return +d.x};
+  var xScale = d3.scale.linear().range([0, width]);
+  //xMap = function(d) { return xScale(xValue(d));}, // data -> display
+  var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
 
+  //var yValue = function(d) { return +d.y};
+  var yScale = d3.scale.linear().range([height, 0]);
+  //yMap = function(d) { return yScale(yValue(d));}, // data -> display
+  var yAxis = d3.svg.axis().scale(yScale).orient("left");
 
+  xScale.domain([xmin-1, xmax+1]);
+  yScale.domain([ymin-1, ymax+1]);
+
+  var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // x-axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("PC1");
+
+    // y-axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("PC2");
+
+    // draw dots
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", function(d) {return xScale(d[0])})
+        .attr("cy", function(d) {return yScale(d[1])})
+        .style("fill", function(d) {return color[d[2]]});
 
 };
